29a762012f606e5ecde390d044f6cb361107969b
vladatman <55131890+vladatman@users.noreply.github.com>
2021-03-20 15:33:02 +0200

    Added Table3x3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner myObj = new Scanner(System.in);
+        boolean game = true;
+        while(game){
+            System.out.println("Select option");
+            int input = Integer.parseInt(myObj.nextLine());
+            // method
+            switch (input) {
+                case 0 -> System.out.println("Starting 3x3");
+                case 1 -> game = false;
+            }
+        }
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,14 @@
 public abstract class Table {
 
-    protected String size;
+    protected String size;  //3x3 or 4x4
+
+    public String getSize() {
+        return this.size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
 
 }
