d39550d11653df7c27d765c36a04120c0c1d3ab0
Artem <58528536+dickusbigus@users.noreply.github.com>
2021-03-22 16:44:01 +0600

    Menu structure initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
         boolean game = true;
         Board4x4 board = new Board4x4();
         while(game){
-            System.out.println("Select option");
+            System.out.println("Welcome! \n If you want to play please select the board size below. ");
+            System.out.println("Select option:\n 1 - 3x3 \n 2- 4x4");
             int input = Integer.parseInt(myObj.nextLine());
             // method
             switch (input) {
@@ -15,6 +16,7 @@ public class Main {
                 case 1 -> board.shuffleBoard();
                 case 2 -> board.printBoard();
                 case 3 -> game = false;
+
             }
         }
     }
