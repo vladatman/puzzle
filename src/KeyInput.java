3c909bcdc1782c019e957609d69e36c1fb051775
vladatman <55131890+vladatman@users.noreply.github.com>
2021-03-31 21:37:31 +0300

    Added check function

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -5,11 +5,17 @@ import java.awt.event.KeyEvent;
 public class KeyInput implements KeyListener {
 
     private Board board;
+    private JFrame frame;
     private JTextArea textArea;
     Move move;
 
-    public KeyInput (Board board, JTextArea textArea) {
+    public KeyInput (Board board, JTextArea textArea, JFrame frame) {
         this.textArea = textArea;
+        this.board = board;
+        this.frame = frame;
+        this.move = new Move(board);
+    }
+    public KeyInput (Board board) {
         this.board = board;
         this.move = new Move(board);
     }
@@ -26,22 +32,18 @@ public class KeyInput implements KeyListener {
 
         //pressing the right arrow
         if(key == KeyEvent.VK_RIGHT){
-            System.out.println("The right arrow key is pressed");
         }
 
         //pressing the left arrow
         else if(key == KeyEvent.VK_LEFT){
-            System.out.println("The left arrow key is pressed");
         }
 
         //pressing the up arrow
         else if(key == KeyEvent.VK_UP){
-            System.out.println("The up arrow key is pressed");
         }
 
         //pressing the down arrow
         else if(key == KeyEvent.VK_DOWN){
-            System.out.println("The down arrow key is pressed");
         }
 
     }
@@ -54,24 +56,28 @@ public class KeyInput implements KeyListener {
         if(key == KeyEvent.VK_RIGHT){
             System.out.println("The right arrow key is released");
             move.right();
+            if(board.isSolved()) frame.removeKeyListener(this);
         }
 
         //releasing the left arrow
         else if(key == KeyEvent.VK_LEFT){
             System.out.println("The left arrow key is released");
             move.left();
+            if(board.isSolved()) frame.removeKeyListener(this);
         }
 
         //releasing the up arrow
         else if(key == KeyEvent.VK_UP){
             System.out.println("The up arrow key is released");
             move.up();
+            if(board.isSolved()) frame.removeKeyListener(this);
         }
 
         //releasing the down arrow
         else if(key == KeyEvent.VK_DOWN){
             System.out.println("The down arrow key is released");
             move.down();
+            if(board.isSolved()) frame.removeKeyListener(this);
         }
 
         textArea.setText(board.printBoard());
