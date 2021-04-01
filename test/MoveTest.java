28aab82f8a12a9d9904f2a4bc02c38312ec48c8e
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-04-01 19:56:53 +0200

    Started MoveTest

--- test/MoveTest.java
+++ test/MoveTest.java
@@ -1,4 +1,30 @@
-
 public class MoveTest {
+    @Test
+    void up(){
+        Board3x3 board1 = new Board3x3();
+        Board4x4 board2 = new Board4x4();
 
+
+    }
+    @Test
+    void down(){
+        Board3x3 board1 = new Board3x3();
+        Board4x4 board2 = new Board4x4();
+
+
+    }
+    @Test
+    void left(){
+        Board3x3 board1 = new Board3x3();
+        Board4x4 board2 = new Board4x4();
+
+
+    }
+    @Test
+    void right(){
+        Board3x3 board1 = new Board3x3();
+        Board4x4 board2 = new Board4x4();
+
+
+    }
 }
