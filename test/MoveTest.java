a06e6b9086aae21566fb216366cd39fc8ed501fe
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-04-01 20:08:53 +0200

    Updated MoveTest() - still trying!

--- test/MoveTest.java
+++ test/MoveTest.java
@@ -2,29 +2,25 @@ public class MoveTest {
     @Test
     void up(){
         Board3x3 board1 = new Board3x3();
-        Board4x4 board2 = new Board4x4();
-
-
+        Move move1 = new Move();
+        assertEquals(move1.up(board1));
     }
     @Test
     void down(){
         Board3x3 board1 = new Board3x3();
-        Board4x4 board2 = new Board4x4();
-
-
+        Move move1 = new Move();
+        assertEquals(move1.down(board1));
     }
     @Test
     void left(){
         Board3x3 board1 = new Board3x3();
-        Board4x4 board2 = new Board4x4();
-
-
+        Move move1 = new Move();
+        assertEquals(move1.left(board1));
     }
     @Test
     void right(){
         Board3x3 board1 = new Board3x3();
-        Board4x4 board2 = new Board4x4();
-
-
+        Move move1 = new Move();
+        assertEquals(move1.right(board1));
     }
 }
